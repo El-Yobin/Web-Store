935c81ccb062f38204d152dbe0c0d1192bc6b748
El-Yobin <El-Yobin@users.noreply.github.com>
2020-03-07 14:55:07 +0300

    yes

--- category__women.js
+++ category__women.js
@@ -1,60 +1,66 @@
 const getProducts = async (url, params) => {
-    const paramsStrings = params ? Object.entries(params).map(([param, value]) => `${param}=${value}`) : [];
+  const paramsStrings = params
+    ? Object.entries(params).map(([param, value]) => `${param}=${value}`)
+    : [];
 
-    const paramsString = paramsStrings.length ? `/?${paramsStrings.join('&')}` : '';
+  const paramsString = paramsStrings.length
+    ? `/?${paramsStrings.join("&")}`
+    : "";
 
-    const urlString = `http://localhost:4000/${url}${paramsString}`;
+  const urlString = `http://localhost:4000/${url}${paramsString}`;
 
-    const getProducts = async () => fetch(urlString);
+  const getProducts = async () => fetch(urlString);
 
-    return getProducts().then(r => r.json())
-}
+  return getProducts().then(r => r.json());
+};
 
 const main = async () => {
+  const productsWomen = await getProducts("api/products", {
+    category: "Women"
+  });
 
-    const productsWomen = await getProducts('api/products', {
-        category: 'Women',
-    })
-
-    console.log(productsWomen);
-    fillGallery(productsWomen);
-}
+  console.log(productsWomen);
+  fillGallery(productsWomen);
+};
 
 main();
 
 function fillGallery(products) {
+  const wrapper = document.querySelector(".categoryMen__wrapper");
+  const container = document.createElement("div");
+  container.setAttribute("class", "products");
 
-    const wrapper = document.querySelector('.categoryMen__wrapper')
-    const container = document.createElement('div');
-    container.setAttribute('class', 'products');
+  for (let i = 0; i < products.length; i++) {
+    const containerWrapper = document.createElement("div");
+    containerWrapper.setAttribute("class", "product");
 
-    for (let i = 0; i < products.length; i++) {
-        const containerWrapper = document.createElement('div');
-        containerWrapper.setAttribute('class', 'product');
+    const imageContainer = document.createElement("div");
+    imageContainer.setAttribute("class", "product__imgContainer");
+    containerWrapper.append(imageContainer);
 
-        const image = document.createElement('img');
-        image.setAttribute('class', 'product__img');
-        image.setAttribute('src', `images\/${products[i].id_model}.jpg`)
-        containerWrapper.append(image);
+    const image = document.createElement("div");
+    image.setAttribute("class", "product__img");
+    image.setAttribute(
+      "style",
+      `background-image: url(images\/${products[i].id_model}.jpg)`
+    );
+    imageContainer.append(image);
 
-        const discription = document.createElement('div');
-        discription.setAttribute('class', 'product__discription');
+    const discription = document.createElement("div");
+    discription.setAttribute("class", "product__discription");
 
-        const title = document.createElement('h4');
-        title.innerHTML = products[i].name;
-        discription.append(title);
+    const title = document.createElement("h4");
+    title.innerHTML = products[i].name;
+    discription.append(title);
 
-        const price = document.createElement('h6');
-        price.innerHTML = `${products[i].price} $`;
-        discription.append(price);
+    const price = document.createElement("h6");
+    price.innerHTML = `${products[i].price} $`;
+    discription.append(price);
 
-        containerWrapper.append(discription);
+    containerWrapper.append(discription);
 
-        container.append(containerWrapper);
-    }
+    container.append(containerWrapper);
+  }
 
-    wrapper.append(container);
+  wrapper.append(container);
 }
-
-
-
--- server/index.js
+++ server/index.js
@@ -1,53 +1,62 @@
-const path = require('path');
-const express = require('express');
-const connect = require('connect');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const session = require('express-session');
-const _ = require('lodash');
-const db = require('./dbConnect');
+const path = require("path");
+const express = require("express");
+const connect = require("connect");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const _ = require("lodash");
+const db = require("./dbConnect");
 
 const app = express();
 
-app.set('port', process.env.PORT || 4000);
+app.set("port", process.env.PORT || 4000);
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, "build")));
 
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "origin, content-type, accept");
 
-    next();
+  next();
 });
 
 app.use(connect().use(cookieParser()));
 app.use(
-    connect().use(
-        session({
-            secret: 'work hard',
-            resave: true,
-            saveUninitialized: false
-        })
-    )
+  connect().use(
+    session({
+      secret: "work hard",
+      resave: true,
+      saveUninitialized: false
+    })
+  )
 );
 
-app.get('/api/products', (req, res) => {
-    if (req.query.category) {
-        db.any(`select * from product where category='${req.query.category}'`)
-            .then(products => res.send(products))
-            .catch(error => console.error(error));
-    }
+app.get("/api/products", (req, res) => {
+  const { category, country, type, style } = req.query;
+
+  if (category) {
+    db.any(`select * from product where category='${category}'`)
+      .then(products => res.send(products))
+      .catch(error => console.error(error));
+  }
+
+  // if (country) {
+  //   db.any(
+  //     `select * from product where country='${country}' and
+  //   type='${type}'`
+  //   )
+  //     .then(products => res.send(products))
+  //     .catch(error => console.error(error));
+  // }
 });
 
-app.get('*', (req, res) => {
-    console.log('server get *');
-    res.send(
-        'Server is working. Please post at "/contact" to submit a message.'
-    );
+app.get("*", (req, res) => {
+  console.log("server get *");
+  res.send('Server is working. Please post at "/contact" to submit a message.');
 });
 
-app.listen(app.get('port'));
+app.listen(app.get("port"));
